f478975848922bf2030f5e88b476418695ccbd9d
shijunfeng <johnshiluck@gmail.comgit>
2017-07-06 08:21:40 +0800

    user can upload resumes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "jobs#index"
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
 
   namespace :admin do
     resources :jobs do
